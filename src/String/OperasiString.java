d792417ab068c1e8441d46dfc872f6bdc9594689
Rinoldi Rasenggan 123 <62507536+RinoldiRasenggan@users.noreply.github.com>
2020-04-15 23:32:55 +0800

    touppercase untuk tolowercase untuk substring(5) untuk substring(0,4) untuk

--- src/String/OperasiString.java
+++ src/String/OperasiString.java
@@ -1,10 +1,20 @@
 package String;
 
+import javax.print.DocFlavor;
+
 public class OperasiString {
     public static void(string[] args) {
         String kota = "Yogyakarta";
 
 
         System.out.println(kota);
+        CHAR{} uniskaChar = { 'U','N','I','S','K','A'};
+        STRING uniskaString = new String(uniskaChar);
+        Sytem.out.println(uniskaString);
+
+        system.out.println(kota.toUpperCase());
+        system.out.println(kota.toLowerCase();
+        system.out.println(kota.substring(5);
+        system.out.println(kota.substring(0,4));
     }
 }
