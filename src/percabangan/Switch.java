5ce3d79076fc85365808d93aad38b349af5784be
Rinoldi Rasenggan 123 <62507536+RinoldiRasenggan@users.noreply.github.com>
2020-04-16 00:12:56 +0800

    pecabangan case sarangheo

---
+++ src/percabangan/Switch.java
@@ -0,0 +1,29 @@
+package percabangan;
+
+public class Switch
+public class static void (String[] args) {
+    char nilai='D';
+    switch(nilai){
+        case 'A':
+            System.out.println("Mhs   : \"Terima kasih pak\"");
+        System.out.println("DOSEN   : \"SELAMAT YA\"");
+        break;
+        case 'B':
+            System.out.println("Mhs   : \"kenapa ulun kada dapat a pak\"");
+        System.out.println("dosen  : \"kayapa yu lah ngalih menyambatnya nah \"");
+        break;
+        case 'C' :
+        System.out.println("Mhs   : \"ULUN TURUS PUL  PAK AI,TUGAS PUL JUA\"");
+        System.out.println("dosen   : \"TAPI BISA  LAH MENJAWAB UJIAN? \"");
+        System.out.println("Mhs   : \"kenapa ulun kada dapat a pak\"");
+        System.out.println("dosen  : \"KAYAPA YU LAH NGALIH MENYAMBATNYA NAH\"");
+        break;
+        default;
+          System.out.println("Mhs   : \"ULUN TURUS PUL  PAK AI,TUGAS PUL JUA\"");
+                  System.out.println("dosen  : \"TAPI BISA  LAH MENJAWAB UJIAN? \"");
+                  System.out.println("dosen : \"kenapa ulun kada dapat a pak\"");
+                  System.out.println("dosen  : \"KAYAPA YU LAH NGALIH MENYAMBATNYA NAH\"");break;
+                  ]
+        }
+}
+
